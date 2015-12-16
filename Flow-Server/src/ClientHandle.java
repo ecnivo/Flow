ad20c5909349c28908ec67ec8af20a2f4c9315f0
Bimesh De Silva <bimeshde@gmail.com>
2015-12-16 12:32:54 -0500

    Added more class tem plates + updated .gitignore

---
+++ Flow-Server/src/ClientHandle.java
@@ -0,0 +1,14 @@
+import java.net.Socket;
+
+public class ClientHandle implements Runnable {
+
+	private Socket socket;
+	private 
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
