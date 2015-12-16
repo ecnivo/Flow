b4887cc8ce490fabee3ecaaa4328f67411b8ee46
Bimesh De Silva <bimeshde@gmail.com>
2015-12-16 12:24:42 -0500

    Created baseline classes

---
+++ Flow-Client/src/FlowClient.java
@@ -0,0 +1,9 @@
+
+public class FlowClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Flow-Server/src/FlowServer.java
@@ -0,0 +1,9 @@
+
+public class FlowServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
