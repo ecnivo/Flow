41723caa8592ec6ea22404867f44b21eb555ca9a
Netdex <netdex593@gmail.com>
2015-12-17 12:01:39 -0500

    Updated build paths to use dependancies

--- Flow-Server/src/ClientHandle.java
+++ Flow-Server/src/ClientHandle.java
@@ -1,14 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class ClientHandle implements Runnable {
 
-	private Socket socket;
-	private User user;
+    private Socket socket;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+    private BufferedReader in;
+    private PrintStream out;
 
-	}
+    private User user;
+
+    public ClientHandle(Socket socket) throws IOException {
+        this.socket = socket;
+
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintStream(socket.getOutputStream());
+    }
+
+    @Override
+    public void run() {
+
+        while (socket.isConnected()) {
+
+        }
+
+    }
 
 }
