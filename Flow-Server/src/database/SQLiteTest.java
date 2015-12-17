98e0a8ad6267a41fe91509d0ed9ba3d5f92b8acc
Bimesh De Silva <bimeshde@gmail.com>
2015-12-17 14:10:14 -0500

    Implemented easier to user querying / updating system

--- Flow-Server/src/database/SQLiteTest.java
+++ Flow-Server/src/database/SQLiteTest.java
@@ -15,22 +15,29 @@ public class SQLiteTest {
 		BufferedReader in = new BufferedReader(
 				new InputStreamReader(System.in));
 
-//		while (true) {
-//			database.update(in.readLine());
-//		}
-
 		while (true) {
-			ResultSet results = database.query(in.readLine());
-			ResultSetMetaData meta = results.getMetaData();
-			int cols = meta.getColumnCount();
-			while (results.next()) {
-				for (int i = 1; i <= cols; i++) {
-					System.out.print(meta.getColumnName(i) + " : "
-							+ results.getString(i));
-					if (i < cols)
-						System.out.print(", ");
+			switch (in.readLine().toLowerCase()) {
+			case "a":
+				database.update(in.readLine());
+				break;
+			case "b":
+				ResultSet results = database.query(in.readLine());
+				ResultSetMetaData meta = results.getMetaData();
+				int cols = meta.getColumnCount();
+				while (results.next()) {
+					for (int i = 1; i <= cols; i++) {
+						System.out.print(meta.getColumnName(i) + " : "
+								+ results.getString(i));
+						if (i < cols)
+							System.out.print(", ");
+					}
+					System.out.println();
 				}
-				System.out.println();
+				break;
+			default:
+				System.out.println(
+						"Enter 'a' for update (i.e. insert, drop, delete, create, etc. Or select 'b' for a query.");
+				break;
 			}
 		}
 	}
