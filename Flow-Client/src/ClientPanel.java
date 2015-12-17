869c18d009d3201bd988db55817419c38783b534
Vince Ou <vince.ou@gmail.com>
2015-12-17 12:44:27 -0500

    Created empty classes for the GUI

--- Flow-Client/src/ClientPanel.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JPanel;
-
-class ClientPanel extends JPanel {
-
-	private DocumentTree docTree;
-	private SettingsPane settings;
-}
--- Flow-Client/src/SettingsPane.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.JPanel;
-
-class SettingsPane extends JPanel {
-
-}
