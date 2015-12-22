995935b73a2cb4882248ef419fbcb9a60da6824d
Netdex <netdex593@gmail.com>
2015-12-22 16:08:51 -0500

    Updated more documentation

--- Flow-Commons/src/message/Message.java
+++ Flow-Commons/src/message/Message.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 /**
+ * Represents a message with an unlimited amount of properties
+ *
  * Created by Netdex on 12/22/2015.
  */
 public class Message implements Serializable {
@@ -14,10 +16,21 @@ public class Message implements Serializable {
         this.stringObjectHashMap = new HashMap<>();
     }
 
-    public void put(String key, Object o){
+    /**
+     * Adds a property to this message
+     * @param key The key to retrieve the property with
+     * @param o The value of the property, must be serializable!
+     */
+    public void put(String key, Serializable o){
         stringObjectHashMap.put(key, o);
     }
 
+    /**
+     * Retrieves a property from this message
+     * @param key The key of the message
+     * @param type The class type of the message, for example String.class
+     * @return The value of the property of type 'type'
+     */
     public Object get(String key, Class<?> type){
         return type.cast(stringObjectHashMap.get(key));
     }
