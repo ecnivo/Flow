63f8cef7c0e61aff64efaf107f083bfcdc9c1527
Netdex <netdex593@gmail.com>
2015-12-27 23:28:08 -0500

    Multithreading of requests

--- Flow-Server/src/server/FlowServer.java
+++ Flow-Server/src/server/FlowServer.java
@@ -1,45 +1,58 @@
 package server;
 
+import database.SQLDatabase;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.UUID;
 
-import database.SQLDatabase;
-
 public class FlowServer implements Runnable {
 
-	private static final int PORT = 10244;
+    private static final int PORT = 10244;
 
-	private SQLDatabase database;
+    private SQLDatabase database;
 
-	public FlowServer() {
+    private static final int MAX_THREADS = 100;
+    private Thread[] threadPool = new Thread[MAX_THREADS];
 
-	}
+    public FlowServer() {
 
-	@Override
-	public void run() {
-		try {
-			ServerSocket serverSocket = new ServerSocket(PORT);
-			while (serverSocket.isBound()) {
-				Socket socket = serverSocket.accept();
-				ClientRequestHandle handle = new ClientRequestHandle(socket);
-				handle.run();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
-	public static void main(String[] args) {
-		FlowServer server = new FlowServer();
-		new Thread(server).start();
-	}
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            while (serverSocket.isBound()) {
+                Socket socket = serverSocket.accept();
 
-	protected UUID newSession() {
-		return UUID.randomUUID();
-	}
+                int i;
+                do {
+                    for (i = 0; i < MAX_THREADS && threadPool[i] != null; i++)
+                        if (!threadPool[i].isAlive())
+                            threadPool[i] = null;
+                } while (threadPool[i] != null);
 
-	protected SQLDatabase getDatabase() {
-		return this.database;
-	}
+                System.err.println("Request assigned worker thread " + i);
+                Thread t = new Thread(new ClientRequestHandle(socket));
+                t.start();
+                threadPool[i] = t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FlowServer server = new FlowServer();
+        new Thread(server).start();
+    }
+
+    protected UUID newSession() {
+        return UUID.randomUUID();
+    }
+
+    protected SQLDatabase getDatabase() {
+        return this.database;
+    }
 }
