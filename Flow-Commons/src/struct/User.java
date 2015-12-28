14373367ccd33ad8040fb0f6f548f627011994ef
Bimesh De Silva <bimeshde@gmail.com>
2015-12-28 09:21:19 -0500

    Removed user UUID

--- Flow-Commons/src/struct/User.java
+++ Flow-Commons/src/struct/User.java
@@ -1,36 +1,29 @@
 package struct;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 /**
  * Created by Netdex on 12/24/2015.
  */
 public class User implements Serializable {
 
-    private String username;
-    private transient String password;
-    private UUID uuid;
+	private String username;
+	private transient String password;
 
-    public User(String username, String password){
-        this.username = username;
-        this.password = password;
-        this.uuid = UUID.randomUUID();
-    }
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
-    public User(String username){
-        this(username, null);
-    }
+	public User(String username) {
+		this(username, null);
+	}
 
-    public String getUsername(){
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getPassword(){
-        return password;
-    }
-
-    public UUID getUUID(){
-        return uuid;
-    }
+	public String getPassword() {
+		return password;
+	}
 }
