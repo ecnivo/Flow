a4e486a8797f2153a6f94fbbe43c068036f48607
Bimesh De Silva <bimeshde@gmail.com>
2015-12-27 23:20:30 -0500

    Merged Server classes

--- Flow-Server/src/server/ClientRequestHandle.java
+++ Flow-Server/src/server/ClientRequestHandle.java
@@ -1,60 +1,62 @@
 package server;
 
-import message.Data;
-import network.DataSocket;
-
 import java.io.IOException;
 import java.net.Socket;
 import java.util.UUID;
 
+import message.Data;
+import network.DataSocket;
+
 public class ClientRequestHandle implements Runnable {
 
-    private Socket socket;
-    private DataSocket psocket;
+	private Socket socket;
+	private DataSocket psocket;
+	private FlowServer server;
 
-    private UUID uuid;
+	private UUID uuid;
 
-    public ClientRequestHandle(Socket socket) throws IOException {
-        this.socket = socket;
-        this.psocket = new DataSocket(socket);
-        this.uuid = UUID.randomUUID();
-    }
+	public ClientRequestHandle(Socket socket) throws IOException {
+		this.socket = socket;
+		this.psocket = new DataSocket(socket);
+		this.uuid = UUID.randomUUID();
+	}
 
-    @Override
-    public void run() {
-        try {
-            socket.setSoTimeout(100);
-            Data data = psocket.receive(Data.class);
-            Data returnData = new Data();
-            switch (data.getType()) {
-                case "login":
-                    String username = data.get("username", String.class);
-                    String password = data.get("password", String.class);
-                    // TODO query the database
-                    if (false) {
-                        UUID sessionID = UUID.randomUUID();
-                        returnData.put("status", "OK");
-                        returnData.put("session_id", sessionID);
-                        // TODO write session ID into database
-                    } else {
-                        returnData.put("status", "INVALID_CREDENTIALS");
-                    }
-                    break;
-                case "user":
-                    String userCmdType = data.get("user_type", String.class);
-                    switch (userCmdType) {
-                        case "REGISTER":
-                            break;
-                        case "CLOSE_ACCOUNT":
-                            break;
-                        case "CHANGE_PASSWORD":
-                            break;
-                    }
-                    break;
-            }
-            psocket.send(returnData);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void run() {
+		try {
+			socket.setSoTimeout(100);
+			Data data = psocket.receive(Data.class);
+			Data returnData = new Data();
+			switch (data.getType()) {
+			case "login":
+				String username = data.get("username", String.class);
+				String password = data.get("password", String.class);
+				// TODO query the database
+				if (this.server.getDatabase().authenticate(username,
+						password)) {
+					UUID sessionID = this.server.newSession();
+					returnData.put("status", "OK");
+					returnData.put("session_id", sessionID);
+					// TODO write session ID into database
+				} else {
+					returnData.put("status", "INVALID_CREDENTIALS");
+				}
+				break;
+			case "user":
+				String userCmdType = data.get("user_type", String.class);
+				switch (userCmdType) {
+				case "REGISTER":
+					break;
+				case "CLOSE_ACCOUNT":
+					break;
+				case "CHANGE_PASSWORD":
+					break;
+				}
+				break;
+			}
+			psocket.send(returnData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Flow-Server/src/server/FlowServer.java
+++ Flow-Server/src/server/FlowServer.java
@@ -1,36 +1,45 @@
 package server;
 
-import database.SQLDatabase;
-
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.UUID;
+
+import database.SQLDatabase;
 
 public class FlowServer implements Runnable {
 
-    private static final int PORT = 10244;
+	private static final int PORT = 10244;
 
-    SQLDatabase database;
+	private SQLDatabase database;
 
-    public FlowServer() {
+	public FlowServer() {
 
-    }
+	}
 
-    @Override
-    public void run() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            while (serverSocket.isBound()) {
-                Socket socket = serverSocket.accept();
-                ClientRequestHandle handle = new ClientRequestHandle(socket);
-                handle.run();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void run() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			while (serverSocket.isBound()) {
+				Socket socket = serverSocket.accept();
+				ClientRequestHandle handle = new ClientRequestHandle(socket);
+				handle.run();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String[] args) {
-        FlowServer server = new FlowServer();
-        new Thread(server).start();
-    }
+	public static void main(String[] args) {
+		FlowServer server = new FlowServer();
+		new Thread(server).start();
+	}
+
+	protected UUID newSession() {
+		return UUID.randomUUID();
+	}
+
+	protected SQLDatabase getDatabase() {
+		return this.database;
+	}
 }
