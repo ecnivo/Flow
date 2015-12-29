a11845453983ffd45b86172652f4a39a33031a2a
Vince Ou <vince.ou@gmail.com>
2015-12-29 10:53:02 -0500

    Added get parent project methods to directory

--- Flow-Commons/src/struct/FlowDirectory.java
+++ Flow-Commons/src/struct/FlowDirectory.java
@@ -8,59 +8,74 @@ import java.util.ArrayList;
  *
  * Created by Netdex on 12/29/2015.
  */
-public class FlowDirectory implements Serializable{
+public class FlowDirectory implements Serializable {
 
     private FlowDirectory parent;
     private ArrayList<FlowDirectory> childDirectories;
     private ArrayList<FlowFile> childFiles;
 
     public FlowDirectory() {
-        this.childDirectories = new ArrayList<>();
-        this.childFiles = new ArrayList<>();
+	this.childDirectories = new ArrayList<>();
+	this.childFiles = new ArrayList<>();
     }
 
     public FlowDirectory(FlowDirectory parent) {
-        this();
-        this.parent = parent;
+	this();
+	this.parent = parent;
     }
 
     /**
      * Gets the parent of this directory, null if root directory
+     * 
      * @return the parent of this directory, null if root directory
      */
     public FlowDirectory getParent() {
-        return parent;
+	return parent;
     }
 
     /**
      * Adds a directory as a child of this one
-     * @param directory The directory to add as a child
+     * 
+     * @param directory
+     *            The directory to add as a child
      */
-    public void addDirectory(FlowDirectory directory){
-        childDirectories.add(directory);
+    public void addDirectory(FlowDirectory directory) {
+	childDirectories.add(directory);
     }
 
     /**
      * Adds a file as a child of this directory
-     * @param file The file to add as a child
+     * 
+     * @param file
+     *            The file to add as a child
      */
-    public void addFile(FlowFile file){
-        childFiles.add(file);
+    public void addFile(FlowFile file) {
+	childFiles.add(file);
     }
 
     /**
      * Gets all child directories
+     * 
      * @return all child directories
      */
     public ArrayList<FlowDirectory> getDirectories() {
-        return childDirectories;
+	return childDirectories;
     }
 
     /**
      * Gets all child files
+     * 
      * @return all child files
      */
     public ArrayList<FlowFile> getFiles() {
-        return childFiles;
+	return childFiles;
+    }
+
+    public FlowProject getProject() {
+	FlowDirectory dir = this;
+	while (!(dir instanceof FlowProject)) {
+	    dir = dir.getParent();
+	}
+	return (FlowProject) dir;
     }
 }
