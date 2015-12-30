7e8782cf57ebecb381828eeb89c673015de4af30
Netdex <netdex593@gmail.com>
2015-12-30 18:31:31 -0500

    Document versions are now managed internally

--- Flow-Commons/src/struct/FlowDocument.java
+++ Flow-Commons/src/struct/FlowDocument.java
@@ -1,6 +1,7 @@
 package struct;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,22 +9,24 @@ import java.util.UUID;
  * <p>
  * Created by Netdex on 12/24/2015.
  */
-public abstract class FlowDocument implements Serializable {
+public abstract class FlowDocument implements Serializable, Comparable<FlowDocument> {
 
     private FlowFile parentFile;
     private UUID uuid;
+    private Date versionDate;
 
-    protected FlowDocument(FlowFile flowFile, UUID uuid) {
+    protected FlowDocument(FlowFile flowFile, UUID uuid, Date versionDate) {
         this.parentFile = flowFile;
         this.uuid = uuid;
+        this.versionDate = versionDate;
     }
 
-    public FlowDocument(FlowFile flowFile) {
-        this(flowFile, UUID.randomUUID());
+    public FlowDocument(FlowFile flowFile, Date versionDate) {
+        this(flowFile, UUID.randomUUID(), versionDate);
     }
 
     public FlowDocument() {
-        this(null, UUID.randomUUID());
+        this(null, UUID.randomUUID(), new Date());
     }
 
     public void setParentFile(FlowFile file) {
@@ -36,6 +39,15 @@ public abstract class FlowDocument implements Serializable {
 
     public UUID getUUID() {
         return uuid;
+    }
+
+    public Date getVersionDate() {
+        return versionDate;
+    }
+
+    @Override
+    public int compareTo(FlowDocument o) {
+        return o.getVersionDate().compareTo(versionDate);
     }
 
 
--- Flow-Commons/src/struct/FlowFile.java
+++ Flow-Commons/src/struct/FlowFile.java
@@ -1,10 +1,7 @@
 package struct;
 
-import java.io.FileNotFoundException;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Map.Entry;
-import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.UUID;
 
 /**
@@ -14,96 +11,74 @@ import java.util.UUID;
  */
 public class FlowFile implements Serializable {
 
-	private FlowDirectory parentDirectory;
-	private UUID uuid;
-	private String fileName;
+    private FlowDirectory parentDirectory;
+    private UUID uuid;
+    private String fileName;
 
-	private transient TreeMap<Date, FlowDocument> versions;
+    private transient TreeSet<FlowDocument> versions;
 
-	public FlowFile(FlowDirectory parentDirectory, String fileName) {
-		this(parentDirectory, fileName, UUID.randomUUID());
-	}
+    public FlowFile(FlowDirectory parentDirectory, String fileName) {
+        this(parentDirectory, fileName, UUID.randomUUID());
+    }
 
-	public FlowFile(FlowDirectory parentDirectory, String fileName, UUID uuid) {
-		this.parentDirectory = parentDirectory;
-		this.uuid = uuid;
-		this.fileName = fileName;
-		this.versions = new TreeMap<>();
-	}
+    public FlowFile(FlowDirectory parentDirectory, String fileName, UUID uuid) {
+        this.parentDirectory = parentDirectory;
+        this.uuid = uuid;
+        this.fileName = fileName;
+        this.versions = new TreeSet<>();
+    }
 
-	/**
-	 * Gets the latest revision of the document stored under this file name
-	 *
-	 * @return the latest revision of the document stored under this file name
-	 */
-	public FlowDocument latest() {
-		return versions.get(versions.lastKey());
-	}
+    /**
+     * Gets the latest revision of the document stored under this file name
+     *
+     * @return the latest revision of the document stored under this file name
+     */
+    public FlowDocument latest() {
+        return versions.last();
+    }
 
-	/**
-	 * Adds a version to the version control system
-	 *
-	 * @param date
-	 *            The timestamp of the modification date of this document
-	 * @param document
-	 *            The document representing the state of the file
-	 */
-	public void addVersion(Date date, FlowDocument document) {
-		versions.put(date, document);
-	}
+    /**
+     * Adds a version to the version control system
+     *
+     * @param document The document representing the state of the file
+     */
+    public void addVersion(FlowDocument document) {
+        versions.add(document);
+    }
 
-	/**
-	 * Gets all versions
-	 *
-	 * @return all versions
-	 */
-	public TreeMap<Date, FlowDocument> getVersions() {
-		return versions;
-	}
+    /**
+     * Gets all versions
+     *
+     * @return all versions
+     */
+    public TreeSet<FlowDocument> getVersions() {
+        return versions;
+    }
 
-	/**
-	 * Gets the parent directory of this file
-	 *
-	 * @return the parent directory of this file
-	 */
-	public FlowDirectory getParentDirectory() {
-		return parentDirectory;
-	}
+    /**
+     * Gets the parent directory of this file
+     *
+     * @return the parent directory of this file
+     */
+    public FlowDirectory getParentDirectory() {
+        return parentDirectory;
+    }
 
-	/**
-	 * Get the date of the specified document. <br>
-	 * This runs in linear time.
-	 * 
-	 * @param document
-	 *            the FlowDocument
-	 * @return the Date associated with the specified FlowDocument
-	 * @throws FileNotFoundException
-	 *             if the specified FlowDocument isn't found
-	 */
-	public Date getVersion(FlowDocument document) throws FileNotFoundException {
-		for (Entry<Date, FlowDocument> entry : versions.entrySet()) {
-			if (document.equals(entry.getValue())) {
-				return entry.getKey();
-			}
-		}
-		throw new FileNotFoundException();
-	}
+    /**
+     * Gets a unique identifier for this file
+     *
+     * @return a unique identifier for this file
+     */
+    public UUID getFileUUID() {
+        return uuid;
+    }
 
-	/**
-	 * Gets a unique identifier for this file
-	 *
-	 * @return a unique identifier for this file
-	 */
-	public UUID getFileUUID() {
-		return uuid;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
-
-	@Override
-	public String toString() {
-		return fileName;
-	}
+    @Override
+    public String toString() {
+        return fileName;
+    }
 }
