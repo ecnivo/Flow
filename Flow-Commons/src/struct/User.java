c2fcd06b821adae27f702903b91396299e7a745d
Vince Ou <vince.ou@gmail.com>
2015-12-30 08:32:39 -0500

    Implemented avatars for Users

--- Flow-Commons/src/struct/User.java
+++ Flow-Commons/src/struct/User.java
@@ -1,6 +1,9 @@
 package struct;
 
+import java.awt.Image;
 import java.io.Serializable;
+
+import javax.swing.ImageIcon;
 
 /**
  * Created by Netdex on 12/24/2015.
@@ -9,21 +12,44 @@ public class User implements Serializable {
 
     private String username;
     private transient String password;
+    private ImageIcon avatar;
+
+    private static final ImageIcon DEFAULT_AVATAR = new ImageIcon(
+	    new ImageIcon("images/icon.png").getImage().getScaledInstance(32,
+		    32, Image.SCALE_SMOOTH));
+
+    public User(String username, String password, ImageIcon avatar) {
+	this.username = username;
+	this.password = password;
+	this.avatar = new ImageIcon(avatar.getImage().getScaledInstance(32, 32,
+		Image.SCALE_SMOOTH));
+    }
 
     public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+	this.username = username;
+	this.password = password;
+	this.avatar = DEFAULT_AVATAR;
     }
 
     public User(String username) {
-        this(username, null);
+	this(username, null);
     }
 
     public String getUsername() {
-        return username;
+	return username;
     }
 
     public String getPassword() {
-        return password;
+	return password;
+    }
+
+    public ImageIcon getAvatar() {
+	return avatar;
+    }
+
+    public void setAvatar(ImageIcon icon) {
+	this.avatar = new ImageIcon(avatar.getImage().getScaledInstance(32, 32,
+		Image.SCALE_SMOOTH));
+
     }
 }
