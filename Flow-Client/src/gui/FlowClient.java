d118b92d8a467f797d77115df1211613f4029564
Vince Ou <vince.ou@gmail.com>
2016-01-07 14:00:00 -0500

    Fixed login issues

--- Flow-Client/src/gui/FlowClient.java
+++ Flow-Client/src/gui/FlowClient.java
@@ -1,57 +1,49 @@
 package gui;
 
-import java.awt.Toolkit;
-import java.io.File;
-import java.io.IOException;
+import shared.Communicator;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
-import shared.Communicator;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 @SuppressWarnings("serial")
 public class FlowClient extends JFrame {
 
     public static final boolean NETWORK = true;
-
-    private static final String HOST = "localhost";
-
-    private static final int PORT = 10244;
-
-    private PanelManager manager;
     public static final javax.swing.border.Border EMPTY_BORDER = BorderFactory.createEmptyBorder(0, 0, 0, 0);
     public static final int BUTTON_ICON_SIZE = 24;
     public static final int SCROLL_SPEED = 12;
-
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, IOException {
-	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	new FlowClient();
-    }
+    private static final String HOST = "10.242.170.164";
+    private static final int PORT = 10244;
+    private PanelManager manager;
 
     public FlowClient() throws IOException {
-	// loads things
-	super("Flow");
+        // loads things
+        super("Flow");
 
-	// sets the icon in the task bar
-	try {
-	    this.setIconImage(ImageIO.read(new File("images/icon.png")));
-	} catch (IOException e) {
-	    JOptionPane.showConfirmDialog(this, "Window icon not found", "Missing Image", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
-	}
+        // sets the icon in the task bar
+        try {
+            this.setIconImage(ImageIO.read(new File("images/icon.png")));
+        } catch (IOException e) {
+            JOptionPane.showConfirmDialog(this, "Window icon not found", "Missing Image", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+        }
 
-	Communicator.initComms(HOST, PORT);
+        Communicator.initComms(HOST, PORT);
 
-	manager = new PanelManager(this);
-	this.add(manager);
+        manager = new PanelManager(this);
+        this.add(manager);
 
-	this.setResizable(true);
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setSize((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.8), (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.8));
-	this.setLocation((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.1), (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.1));
-	this.setVisible(true);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.8), (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.8));
+        this.setLocation((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 0.1), (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() * 0.1));
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, IOException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        new FlowClient();
     }
 }
