31f5691dede39abe55148bf58bc0d91bde3e79ff
Bimesh De Silva <bimeshde@gmail.com>
2016-01-10 12:18:04 -0500

    Added UUID to directory

--- Flow-Commons/src/struct/FlowDirectory.java
+++ Flow-Commons/src/struct/FlowDirectory.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Represents a directory in the flow file system
  * <p>
@@ -13,116 +11,137 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class FlowDirectory implements Serializable {
 
-    private FlowDirectory parent;
-    private ArrayList<FlowDirectory> childDirectories;
-    private ArrayList<FlowFile> childFiles;
+	private FlowDirectory parent;
+	private ArrayList<FlowDirectory> childDirectories;
+	private ArrayList<FlowFile> childFiles;
+	private UUID uuid;
 
-    private String dirName;
+	private String dirName;
 
-    public FlowDirectory(String dirName) {
-	this.childDirectories = new ArrayList<>();
-	this.childFiles = new ArrayList<>();
-	this.dirName = dirName;
-	// TODO check directory name for invalid characters, throw exception if
-	// not
-    }
-
-    public FlowDirectory(FlowDirectory parent, String dirName) {
-	this(dirName);
-	this.parent = parent;
-    }
-
-    public void setDirectoryName(String newName) {
-	this.dirName = newName;
-    }
-
-    /**
-     * Gets the parent of this directory, null if root directory
-     *
-     * @return the parent of this directory, null if root directory
-     */
-    public FlowDirectory getParent() {
-	return parent;
-    }
-
-    /**
-     * Adds a directory as a child of this one
-     *
-     * @param directory
-     *            The directory to add as a child
-     */
-    public void addDirectory(FlowDirectory directory) {
-	childDirectories.add(directory);
-    }
-
-    /**
-     * Adds a file as a child of this directory
-     *
-     * @param file
-     *            The file to add as a child
-     */
-    public void addFile(FlowFile file) throws DuplicateFileNameException {
-	for (FlowFile childFile : childFiles) {
-	    if (childFile.getFileName().equals(file.getFileName()))
-		throw new DuplicateFileNameException();
+	public FlowDirectory(String dirName) {
+		this.childDirectories = new ArrayList<>();
+		this.childFiles = new ArrayList<>();
+		this.dirName = dirName;
+		// TODO check directory name for invalid characters, throw exception if
+		// not
 	}
-	childFiles.add(file);
-    }
 
-    /**
-     * Gets all child directories
-     *
-     * @return all child directories
-     */
-    public ArrayList<FlowDirectory> getDirectories() {
-	return childDirectories;
-    }
-
-    /**
-     * Gets all child files
-     *
-     * @return all child files
-     */
-    public ArrayList<FlowFile> getFiles() {
-	return childFiles;
-    }
-
-    /**
-     * @return the fully qualified path from root directory
-     */
-    public String getFullyQualifiedPath() {
-	String path = "";
-	FlowDirectory cd = this;
-	while (cd.getParent() != null) {
-	    path += cd.getDirectoryName() + "/";
-	    cd = cd.getParent();
+	public FlowDirectory(FlowDirectory parent, String dirName, UUID uuid) {
+		this(dirName, uuid);
+		this.parent = parent;
 	}
-	return path;
-    }
 
-    /**
-     * @return the root directory
-     */
-    public FlowDirectory getRootDirectory() {
-	FlowDirectory cd = this;
-	while (cd.getParent() != null) {
-	    cd = cd.getParent();
+	public FlowDirectory(FlowDirectory parent, String dirName) {
+		this(dirName);
+		this.parent = parent;
+		this.uuid = UUID.randomUUID();
 	}
-	return cd;
-    }
-    
-    public String getDirectoryName() {
-	return dirName;
-    }
 
-    @Override
-    public String toString() {
-	return dirName;
-    }
+	public FlowDirectory(String dirName, UUID uuid) {
+		this(dirName);
+		this.uuid = uuid;
+	}
 
-    static class DuplicateFileNameException extends Exception {
-    }
+	public void setDirectoryName(String newName) {
+		this.dirName = newName;
+	}
 
-    static class InvalidFileNameException extends Exception {
-    }
+	/**
+	 * Gets the uuid of this directory
+	 *
+	 * @return the uuid
+	 */
+	public UUID getDirectoryUUID() {
+		return uuid;
+	}
+
+	/**
+	 * Gets the parent of this directory, null if root directory
+	 *
+	 * @return the parent of this directory, null if root directory
+	 */
+	public FlowDirectory getParent() {
+		return parent;
+	}
+
+	/**
+	 * Adds a directory as a child of this one
+	 *
+	 * @param directory
+	 *            The directory to add as a child
+	 */
+	public void addDirectory(FlowDirectory directory) {
+		childDirectories.add(directory);
+	}
+
+	/**
+	 * Adds a file as a child of this directory
+	 *
+	 * @param file
+	 *            The file to add as a child
+	 */
+	public void addFile(FlowFile file) throws DuplicateFileNameException {
+		for (FlowFile childFile : childFiles) {
+			if (childFile.getFileName().equals(file.getFileName()))
+				throw new DuplicateFileNameException();
+		}
+		childFiles.add(file);
+	}
+
+	/**
+	 * Gets all child directories
+	 *
+	 * @return all child directories
+	 */
+	public ArrayList<FlowDirectory> getDirectories() {
+		return childDirectories;
+	}
+
+	/**
+	 * Gets all child files
+	 *
+	 * @return all child files
+	 */
+	public ArrayList<FlowFile> getFiles() {
+		return childFiles;
+	}
+
+	/**
+	 * @return the fully qualified path from root directory
+	 */
+	public String getFullyQualifiedPath() {
+		String path = "";
+		FlowDirectory cd = this;
+		while (cd.getParent() != null) {
+			path += cd.getDirectoryName() + "/";
+			cd = cd.getParent();
+		}
+		return path;
+	}
+
+	/**
+	 * @return the root directory
+	 */
+	public FlowDirectory getRootDirectory() {
+		FlowDirectory cd = this;
+		while (cd.getParent() != null) {
+			cd = cd.getParent();
+		}
+		return cd;
+	}
+
+	public String getDirectoryName() {
+		return dirName;
+	}
+
+	@Override
+	public String toString() {
+		return dirName;
+	}
+
+	static class DuplicateFileNameException extends Exception {
+	}
+
+	static class InvalidFileNameException extends Exception {
+	}
 }
