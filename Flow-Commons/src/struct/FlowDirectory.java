085a94653345695f392c0489158f842824433a49
Netdex <netdex593@gmail.com>
2016-01-13 19:19:58 -0500

    Removed parent/child structure, probably broke everything

--- Flow-Commons/src/struct/FlowDirectory.java
+++ Flow-Commons/src/struct/FlowDirectory.java
@@ -11,34 +11,16 @@ import java.util.UUID;
  */
 public class FlowDirectory implements Serializable {
 
-    private FlowDirectory parent;
-    private transient ArrayList<FlowDirectory> childDirectories;
-    private transient ArrayList<FlowFile> childFiles;
     private UUID uuid;
 
     private String dirName;
 
     public FlowDirectory(String dirName) {
-        this.childDirectories = new ArrayList<>();
-        this.childFiles = new ArrayList<>();
-        this.dirName = dirName;
-        // TODO check directory name for invalid characters, throw exception if
-        // not
-    }
-
-    public FlowDirectory(FlowDirectory parent, String dirName, UUID uuid) {
-        this(dirName, uuid);
-        this.parent = parent;
-    }
-
-    public FlowDirectory(FlowDirectory parent, String dirName) {
-        this(dirName);
-        this.parent = parent;
-        this.uuid = UUID.randomUUID();
+        this(dirName, UUID.randomUUID());
     }
 
     public FlowDirectory(String dirName, UUID uuid) {
-        this(dirName);
+        this.dirName = dirName;
         this.uuid = uuid;
     }
 
@@ -53,86 +35,6 @@ public class FlowDirectory implements Serializable {
      */
     public UUID getDirectoryUUID() {
         return uuid;
-    }
-
-    /**
-     * Gets the parent of this directory, null if root directory
-     *
-     * @return the parent of this directory, null if root directory
-     * @deprecated
-     */
-    private FlowDirectory getParent() {
-        return parent;
-    }
-
-    /**
-     * Adds a directory as a child of this one
-     *
-     * @param directory The directory to add as a child
-     * @deprecated
-     */
-    private void addDirectory(FlowDirectory directory) {
-        childDirectories.add(directory);
-    }
-
-    /**
-     * Adds a file as a child of this directory
-     *
-     * @param file The file to add as a child
-     * @deprecated
-     */
-    private void addFile(FlowFile file) throws DuplicateFileNameException {
-        for (FlowFile childFile : childFiles) {
-            if (childFile.getFileName().equals(file.getFileName()))
-                throw new DuplicateFileNameException();
-        }
-        childFiles.add(file);
-    }
-
-    /**
-     * Gets all child directories
-     *
-     * @return all child directories
-     * @deprecated
-     */
-    private ArrayList<FlowDirectory> getDirectories() {
-        return childDirectories;
-    }
-
-    /**
-     * Gets all child files
-     *
-     * @return all child files
-     * @deprecated
-     */
-    private ArrayList<FlowFile> getFiles() {
-        return childFiles;
-    }
-
-    /**
-     * @return the fully qualified path from root directory
-     * @deprecated
-     */
-    private String getFullyQualifiedPath() {
-        String path = "";
-        FlowDirectory cd = this;
-        while (cd.getParent() != null) {
-            path += cd.getDirectoryUUID() + "/";
-            cd = cd.getParent();
-        }
-        return path;
-    }
-
-    /**
-     * @return the root directory
-     * @deprecated
-     */
-    private FlowDirectory getRootDirectory() {
-        FlowDirectory cd = this;
-        while (cd.getParent() != null) {
-            cd = cd.getParent();
-        }
-        return cd;
     }
 
     public String getDirectoryName() {
