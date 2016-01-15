7289ad22e843b68c5d10c84431bd7086f4e21118
Netdex <netdex593@gmail.com>
2016-01-15 16:03:10 -0500

    Fixed results taking first column instead of first row Fixes #43

--- Flow-Server/src/util/Results.java
+++ Flow-Server/src/util/Results.java
@@ -6,73 +6,78 @@ import java.util.LinkedList;
 
 public class Results {
 
-	public static String[][] toStringArray(String[] columnNames,
-			ResultSet results) throws SQLException {
-		LinkedList<String[]> list = new LinkedList<>();
-		while (results.next()) {
-			String[] row = new String[columnNames.length];
-			for (int i = 0; i < row.length; i++) {
-				row[i] = results.getString(columnNames[i]);
-			}
-			list.add(row);
-		}
-		return list.toArray(new String[][] { {} });
-	}
+    public static String[][] toStringArray(String[] columnNames,
+                                           ResultSet results) throws SQLException {
+        LinkedList<String[]> list = new LinkedList<>();
+        while (results.next()) {
+            String[] row = new String[columnNames.length];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = results.getString(columnNames[i]);
+            }
+            list.add(row);
+        }
+        return list.toArray(new String[][]{{}});
+    }
 
-	public static String[] toStringArray(String columnName, ResultSet results)
-			throws SQLException {
-		return toStringArray(new String[] { columnName }, results)[0];
-	}
+    public static String[] toStringArray(String columnName, ResultSet results)
+            throws SQLException {
+        String[][] arr = toStringArray(new String[]{columnName}, results);
+        String[] retarr = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            retarr[i] = arr[i][0];
+        }
+        return retarr;
+    }
 
-	public static int[][] toIntArray(String[] columnNames, ResultSet results)
-			throws SQLException {
-		LinkedList<int[]> list = new LinkedList<>();
-		while (results.next()) {
-			int[] row = new int[columnNames.length];
-			for (int i = 0; i < row.length; i++) {
-				row[i] = results.getInt(columnNames[i]);
-			}
-			list.add(row);
-		}
-		return list.size() > 0 ? (int[][]) list.toArray() : new int[][] { {} };
-	}
+    public static int[][] toIntArray(String[] columnNames, ResultSet results)
+            throws SQLException {
+        LinkedList<int[]> list = new LinkedList<>();
+        while (results.next()) {
+            int[] row = new int[columnNames.length];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = results.getInt(columnNames[i]);
+            }
+            list.add(row);
+        }
+        return list.size() > 0 ? (int[][]) list.toArray() : new int[][]{{}};
+    }
 
-	public static int[] toIntArray(String columnName, ResultSet results)
-			throws SQLException {
-		return toIntArray(new String[] { columnName }, results)[0];
-	}
+    public static int[] toIntArray(String columnName, ResultSet results)
+            throws SQLException {
+        return toIntArray(new String[]{columnName}, results)[0];
+    }
 
-	public static String[][] toStringArray(int[] columnNumbers,
-			ResultSet results) throws SQLException {
-		LinkedList<String[]> list = new LinkedList<>();
-		while (results.next()) {
-			String[] row = new String[columnNumbers.length];
-			for (int i = 0; i < row.length; i++) {
-				row[i] = results.getString(columnNumbers[i]);
-			}
-			list.add(row);
-		}
-		return list.size() > 0 ? list.toArray(new String[][] { {} })
-				: new String[][] { {} };
-	}
+    public static String[][] toStringArray(int[] columnNumbers,
+                                           ResultSet results) throws SQLException {
+        LinkedList<String[]> list = new LinkedList<>();
+        while (results.next()) {
+            String[] row = new String[columnNumbers.length];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = results.getString(columnNumbers[i]);
+            }
+            list.add(row);
+        }
+        return list.size() > 0 ? list.toArray(new String[][]{{}})
+                               : new String[][]{{}};
+    }
 
-	public static String[] toStringArray(int columnNumber, ResultSet results)
-			throws SQLException {
-		return toStringArray(new int[] { columnNumber }, results)[0];
-	}
+    public static String[] toStringArray(int columnNumber, ResultSet results)
+            throws SQLException {
+        return toStringArray(new int[]{columnNumber}, results)[0];
+    }
 
-	public static String[][] toStringArray(ResultSet results)
-			throws SQLException {
-		int noOfColumns = results.getMetaData().getColumnCount();
-		LinkedList<String[]> list = new LinkedList<>();
-		while (results.next()) {
-			String[] row = new String[noOfColumns];
-			for (int i = 0; i < row.length; i++) {
-				row[i] = results.getString(i);
-			}
-			list.add(row);
-		}
-		return list.size() > 0 ? list.toArray(new String[][] { {} })
-				: new String[][] { {} };
-	}
+    public static String[][] toStringArray(ResultSet results)
+            throws SQLException {
+        int noOfColumns = results.getMetaData().getColumnCount();
+        LinkedList<String[]> list = new LinkedList<>();
+        while (results.next()) {
+            String[] row = new String[noOfColumns];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = results.getString(i);
+            }
+            list.add(row);
+        }
+        return list.size() > 0 ? list.toArray(new String[][]{{}})
+                               : new String[][]{{}};
+    }
 }
