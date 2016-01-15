a83a3702b1086386963063f8d211ae56e0a1db9e
Bimesh De Silva <bimeshde@gmail.com>
2016-01-15 11:27:43 -0500

    Added documentation to FlowServer

--- Flow-Server/src/server/FlowServer.java
+++ Flow-Server/src/server/FlowServer.java
@@ -1,7 +1,5 @@
 package server;
 
-import database.SQLDatabase;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -11,65 +9,83 @@ import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import java.util.logging.Logger;
 
+import database.SQLDatabase;
+
 public class FlowServer implements Runnable {
 
-    private static Logger L = Logger.getLogger("FlowServer");
-    public static String ERROR = "INTERNAL_SERVER_ERROR";
-    public static final int PORT = 10244;
-    // private static final int ARC_PORT = 10225;
+	private static Logger L = Logger.getLogger("FlowServer");
 
-    private SQLDatabase database;
+	/**
+	 * Represents an error caused by the server, rather than client.
+	 */
+	public static String ERROR = "INTERNAL_SERVER_ERROR";
 
-    private static final int MAX_THREADS = 100;
-    private Thread[] threadPool = new Thread[MAX_THREADS];
+	public static final int PORT = 10244;
 
-    public FlowServer() {
-        this.database = new SQLDatabase("data/FlowDatabse.db");
-    }
+	// NETDEX WHAT IS THIS FOR?
+	// private static final int ARC_PORT = 10225;
 
-    @Override
-    public void run() {
-        DataManagement.getInstance().init(new File("data"));
-        try {
-            L.info("started listening");
-            ServerSocket serverSocket = new ServerSocket(PORT);
+	private SQLDatabase database;
 
-            while (serverSocket.isBound()) {
-                Socket socket = serverSocket.accept();
-                L.info("accepted connection from " + socket.getRemoteSocketAddress());
-                int i = 0;
-                do {
-                    i %= MAX_THREADS;
-                    if (threadPool[i] != null && !threadPool[i].isAlive())
-                        threadPool[i--] = null;
-                    ++i;
-                } while (threadPool[i] != null);
+	// TODO Verify capability when running at max threads
+	private static final int MAX_THREADS = 100;
+	private Thread[] threadPool = new Thread[MAX_THREADS];
 
-                L.info("Request assigned worker thread " + i);
-                Thread t = new Thread(new ClientRequestHandle(this, socket));
-                t.start();
-                threadPool[i] = t;
-            }
-            serverSocket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public FlowServer() {
+		this.database = new SQLDatabase("data/FlowDatabse.db");
+	}
 
-    protected UUID newSession(String username) {
-        UUID sessionId = UUID.randomUUID();
-        this.database.newSession(username, sessionId.toString());
-        return sessionId;
-    }
+	@Override
+	public void run() {
+		DataManagement.getInstance().init(new File("data"));
+		try {
+			L.info("started listening");
+			ServerSocket serverSocket = new ServerSocket(PORT);
 
-    protected SQLDatabase getDatabase() {
-        return this.database;
-    }
+			while (serverSocket.isBound()) {
+				Socket socket = serverSocket.accept();
+				L.info("accepted connection from "
+						+ socket.getRemoteSocketAddress());
+				int i = 0;
+				do {
+					i %= MAX_THREADS;
+					if (threadPool[i] != null && !threadPool[i].isAlive())
+						threadPool[i--] = null;
+					++i;
+				} while (threadPool[i] != null);
 
-    public static void main(String[] args) throws IOException, KeyManagementException, NoSuchAlgorithmException {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%n");
-        FlowServer server = new FlowServer();
-        new Thread(server).start();
-        // TEST CODE
-    }
+				L.info("Request assigned worker thread " + i);
+				Thread t = new Thread(new ClientRequestHandle(this, socket));
+				t.start();
+				threadPool[i] = t;
+			}
+			serverSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected UUID newSession(String username) {
+		UUID sessionId = UUID.randomUUID();
+		this.database.newSession(username, sessionId.toString());
+		return sessionId;
+	}
+
+	/**
+	 * Getter for the associated SQLDatabse instance.
+	 * 
+	 * @return the associated SQLDatabase instance.
+	 */
+	protected SQLDatabase getDatabase() {
+		return this.database;
+	}
+
+	public static void main(String[] args) throws IOException,
+			KeyManagementException, NoSuchAlgorithmException {
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+				"%4$s: %5$s%n");
+		FlowServer server = new FlowServer();
+		new Thread(server).start();
+		// TEST CODE
+	}
 }
