17ef6ac2852f42b15c50779195769640cf7a5eff
Bimesh De Silva <bimeshde@gmail.com>
2016-01-14 21:17:14 -0500

    Added output of results to ServerTester

--- Flow-Server/src/test/ServerTest.java
+++ Flow-Server/src/test/ServerTest.java
@@ -1,46 +1,47 @@
 package test;
 
-import message.Data;
-import network.FMLNetworker;
-import server.FlowServer;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.UUID;
+
+import message.Data;
+import network.FMLNetworker;
+import server.FlowServer;
 
 /**
  * Created by Netdex on 1/14/2016.
  */
 public class ServerTest {
 
-    public static void main(String[] args) throws Exception {
-        /*
-        SMALL UTILITY TO TEST THE FUNCTION OF THE SERVER.
-        READS FROM STANDARD INPUT, AND WRITES TO LOCAL SERVER ON NETWORK.
-
-        SYNTAX:
-        key1=value1;key2=value2;key3=$uuid1
-        ALL ARGUMENTS ARE STRINGS UNLESS PRECEEDED BY A $, WHERE IT BECOMES A UUID.
-
-        EX.
-        type=list_projects;session_id=$3ca58930-967d-437f-8f0f-3ac5eee4429b
-         */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        FMLNetworker fmlNetworker = new FMLNetworker("localhost", FlowServer.PORT);
-        while (true) {
-            String dataStr = br.readLine();
-            Data data = new Data();
-            String[] dataInParse = dataStr.split(";");
-            for (String flag : dataInParse) {
-                String[] flagInParse = flag.split("=");
-                String key = flagInParse[0];
-                Serializable val = flagInParse[1];
-                if (flagInParse[1].startsWith("$"))
-                    val = UUID.fromString(flagInParse[1].substring(1));
-                data.put(key, val);
-            }
-            fmlNetworker.send(data);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		/*
+		 * SMALL UTILITY TO TEST THE FUNCTION OF THE SERVER. READS FROM STANDARD
+		 * INPUT, AND WRITES TO LOCAL SERVER ON NETWORK.
+		 * 
+		 * SYNTAX: key1=value1;key2=value2;key3=$uuid1 ALL ARGUMENTS ARE STRINGS
+		 * UNLESS PRECEEDED BY A $, WHERE IT BECOMES A UUID.
+		 * 
+		 * EX.
+		 * type=list_projects;session_id=$3ca58930-967d-437f-8f0f-3ac5eee4429b
+		 */
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		FMLNetworker fmlNetworker = new FMLNetworker("localhost",
+				FlowServer.PORT);
+		while (true) {
+			String dataStr = br.readLine();
+			Data data = new Data();
+			String[] dataInParse = dataStr.split(";");
+			for (String flag : dataInParse) {
+				String[] flagInParse = flag.split("=");
+				String key = flagInParse[0];
+				Serializable val = flagInParse[1];
+				if (flagInParse[1].startsWith("$"))
+					val = UUID.fromString(flagInParse[1].substring(1));
+				data.put(key, val);
+			}
+			System.out.println(fmlNetworker.send(data));
+		}
+	}
 }
