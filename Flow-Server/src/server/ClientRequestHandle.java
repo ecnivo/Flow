9be871d2b1628a875f156dab467703a512de604e
Bimesh De Silva <bimeshde@gmail.com>
2016-01-15 12:19:57 -0500

    Fixed column name

--- Flow-Server/src/server/ClientRequestHandle.java
+++ Flow-Server/src/server/ClientRequestHandle.java
@@ -1,5 +1,13 @@
 package server;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.logging.Logger;
+
 import database.SQLDatabase;
 import message.Data;
 import network.DataSocket;
@@ -8,14 +16,6 @@ import struct.User;
 import util.DataManipulation;
 import util.DatabaseException;
 import util.Results;
-
-import java.io.IOException;
-import java.net.Socket;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.UUID;
-import java.util.logging.Logger;
 
 public class ClientRequestHandle implements Runnable {
 
@@ -364,8 +364,7 @@ public class ClientRequestHandle implements Runnable {
 							.getFileInfo(fileUUID.toString());
 					returnData.put("file_name",
 							results.getString("DocumentName"));
-					returnData.put("file_type",
-							results.getString("DocumentType"));
+					returnData.put("file_type", results.getString("FileType"));
 					returnData.put("file_versions",
 							DataManipulation.getUUIDsFromArray(this.database
 									.getFileVersions(fileUUID.toString())));
@@ -442,29 +441,31 @@ public class ClientRequestHandle implements Runnable {
 				break;
 			// TODO Implement sending messages to active sessions on changes
 			// ^-- NETDEX
-				case "text_document_modify":
-					UUID projectUUID = data.get("project_uuid", UUID.class);
-					UUID fileUUID = data.get("file_uuid", UUID.class);
-					int line = data.get("line", Integer.class);
-					int idx = data.get("idx", Integer.class);
+			case "text_document_modify":
+				UUID projectUUID = data.get("project_uuid", UUID.class);
+				UUID fileUUID = data.get("file_uuid", UUID.class);
+				int line = data.get("line", Integer.class);
+				int idx = data.get("idx", Integer.class);
 
-					try {
-						UUID latestVersionUUID = UUID.fromString(this.database.getLatestVersionUUID(fileUUID.toString()));
-						TextDocument td = DataManagement.getInstance().getTextDocument(fileUUID, latestVersionUUID);
-						switch (data.get("mod_type", String.class)) {
-							case "INSERT":
-								String str = data.get("str", String.class);
-								for (char c : str.toCharArray()) {
-									td.insert(c, line, idx--);
-								}
-								break;
-							case "DELETE":
-								int len = data.get("len", Integer.class);
-								while (len-- > 0)
-									td.delete(line, idx);
-								break;
+				try {
+					UUID latestVersionUUID = UUID.fromString(this.database
+							.getLatestVersionUUID(fileUUID.toString()));
+					TextDocument td = DataManagement.getInstance()
+							.getTextDocument(fileUUID, latestVersionUUID);
+					switch (data.get("mod_type", String.class)) {
+					case "INSERT":
+						String str = data.get("str", String.class);
+						for (char c : str.toCharArray()) {
+							td.insert(c, line, idx--);
 						}
-					} catch (DatabaseException e) {
+						break;
+					case "DELETE":
+						int len = data.get("len", Integer.class);
+						while (len-- > 0)
+							td.delete(line, idx);
+						break;
+					}
+				} catch (DatabaseException e) {
 
 				}
 				break;
