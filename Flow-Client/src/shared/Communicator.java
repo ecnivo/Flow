fc7ceeac4ac079e4fab7ed180f716e6d75d243d3
Vince Ou <vince.ou@gmail.com>
2016-01-16 13:20:06 -0500

    Fix for #58

--- Flow-Client/src/shared/Communicator.java
+++ Flow-Client/src/shared/Communicator.java
@@ -3,6 +3,8 @@ package shared;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.swing.JOptionPane;
+
 import message.Data;
 import network.FMLNetworker;
 import callback.TextModificationListener;
@@ -14,14 +16,32 @@ public class Communicator {
     private static FMLNetworker networker;
 
     public static void initComms(String host, int port) {
-        networker = new FMLNetworker(host, port, 10225);// TODO hardcoded
+	networker = new FMLNetworker(host, port, 10225);// TODO hardcoded
     }
 
     @SuppressWarnings("unchecked")
     public static Data communicate(Data data) {
+	Thread timer = new Thread(new Runnable() {
+
+	    @Override
+	    public void run() {
+		try {
+		    Thread.sleep(30000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
+	timer.start();
 	try {
 	    return networker.send(data);
 	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	try {
+	    timer.join(30000);
+	} catch (InterruptedException e) {
+	    JOptionPane.showConfirmDialog(null, "Connection to server timed out.", "Connection to server failed", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
 	    e.printStackTrace();
 	}
 	return null;
@@ -34,8 +54,8 @@ public class Communicator {
     public static void setSessionID(UUID sessionID) {
 	Communicator.sessionID = sessionID;
     }
-    
-    public static void initAsync(){
+
+    public static void initAsync() {
 	try {
 	    networker.initAsync();
 	} catch (IOException e) {
@@ -44,18 +64,18 @@ public class Communicator {
     }
 
     public static void addFileChangeListener(TextModificationListener listener, UUID fileUUID) {
-        networker.registerCallbackListener(listener, fileUUID);
+	networker.registerCallbackListener(listener, fileUUID);
     }
 
     public static void removeFileChangeListener(UUID fileUUID) {
-        networker.unregisterCallbackListener(fileUUID);
+	networker.unregisterCallbackListener(fileUUID);
     }
-    
+
     public static String getUsername() {
-        return username;
+	return username;
     }
 
     public static void setUsername(String username) {
-        Communicator.username = username;
+	Communicator.username = username;
     }
 }
