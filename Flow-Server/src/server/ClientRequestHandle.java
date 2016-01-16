faeede1368bcedef09e393ceb15ef3eb74f6553a
Netdex <netdex593@gmail.com>
2016-01-16 10:35:51 -0500

    Added username to file mod event

--- Flow-Server/src/server/ClientRequestHandle.java
+++ Flow-Server/src/server/ClientRequestHandle.java
@@ -1,13 +1,5 @@
 package server;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.UUID;
-import java.util.logging.Logger;
-
 import callback.DocumentCallbackEvent;
 import callback.PersistentHandleManager;
 import database.SQLDatabase;
@@ -18,6 +10,14 @@ import struct.VersionText;
 import util.DataManipulation;
 import util.DatabaseException;
 import util.Results;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 public class ClientRequestHandle implements Runnable {
 
@@ -464,6 +464,7 @@ public class ClientRequestHandle implements Runnable {
 				int idx = data.get("idx", Integer.class);
 
 				try {
+					String username = this.database.getUsername(data.get("session_id", String.class));
 					UUID latestVersionUUID = UUID.fromString(this.database
 							.getLatestVersionUUID(fileUUID.toString()));
 					VersionText td = VersionManager.getInstance()
@@ -476,7 +477,7 @@ public class ClientRequestHandle implements Runnable {
 						}
 						DocumentCallbackEvent event = new DocumentCallbackEvent(
 								DocumentCallbackEvent.DocumentCallbackType.INSERT,
-								latestVersionUUID, line, idx, str, -1);
+								latestVersionUUID, username, line, idx, str, -1);
 						PersistentHandleManager.getInstance()
 								.doCallbackEvent(latestVersionUUID, event);
 					}
@@ -487,7 +488,7 @@ public class ClientRequestHandle implements Runnable {
 							td.delete(line, idx);
 						DocumentCallbackEvent event = new DocumentCallbackEvent(
 								DocumentCallbackEvent.DocumentCallbackType.DELETE,
-								latestVersionUUID, line, idx, null, len);
+								latestVersionUUID, username, line, idx, null, len);
 						PersistentHandleManager.getInstance()
 								.doCallbackEvent(latestVersionUUID, event);
 					}
