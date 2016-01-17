1384ecced06eb83511b53ada78fa33e3c4f592d9
Vince Ou <vince.ou@gmail.com>
2016-01-16 21:12:19 -0500

    Fix #65 and Fix #84

--- Flow-Client/src/shared/DocTree.java
+++ Flow-Client/src/shared/DocTree.java
@@ -135,7 +135,7 @@ public abstract class DocTree extends JTree {
 		    ((DefaultTreeModel) getModel()).removeNodeFromParent((MutableTreeNode) root.getChildAt(i));
 		}
 	    }
-	    ((DefaultTreeModel) getModel()).nodeChanged(root);
+	    // ((DefaultTreeModel) getModel()).nodeChanged(root);
 	    revalidate();
 	    repaint();
 	}
@@ -210,7 +210,7 @@ public abstract class DocTree extends JTree {
 	}
 
 	reloadProjectFilesRecursively(reloadedProject, projectNode);
-	((DefaultTreeModel) getModel()).nodeChanged(projectNode);
+	// ((DefaultTreeModel) getModel()).nodeChanged(projectNode);
     }
 
     private void reloadProjectFilesRecursively(Data remoteParentDir, DirectoryNode localNode) {
