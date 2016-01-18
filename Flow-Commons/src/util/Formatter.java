ddb6c9fd32175d17c3cf717d3a40527c21f5946f
Netdex <netdex593@gmail.com>
2016-01-17 22:20:16 -0500

    Formatter is now functional

--- Flow-Commons/src/util/Formatter.java
+++ Flow-Commons/src/util/Formatter.java
@@ -5,40 +5,39 @@ package util;
  */
 public class Formatter {
 
-    private static String TAB = "  ";
+    private static String TAB = "    ";
 
     public static String format(String str) {
         String[] lines = str.replace("\t", TAB).split("\n");
         String text = "";
         int tabIndex = 0;
         for (String line : lines) {
-            line = line.replaceFirst("^ +", "");
-            for (int i = 0; i < tabIndex; i++)
-                text += TAB;
-            boolean escape = false;
-            boolean inside = false;
-            boolean newEscape = false;
-            for (char c : line.toCharArray()) {
-                switch (c) {
-                    case '\\':
-                        escape = true;
-                        newEscape = true;
-                        break;
-                    case '{':
-                        if (!inside && !escape)
-                            tabIndex++;
-                        break;
-                    case '}':
-                        if (!inside && !escape)
-                            tabIndex--;
-                        break;
-                }
-                if (escape && !newEscape)
-                    escape = false;
-                if (newEscape)
-                    newEscape = false;
+            line = line.trim();
+
+            if (line.startsWith("}") && line.endsWith("{")) {
+                tabIndex--;
+                for (int i = 0; i < tabIndex; i++)
+                    text += TAB;
+                text += line + '\n';
+                tabIndex++;
+            } else if (line.startsWith("}")) {
+                tabIndex--;
+                for (int i = 0; i < tabIndex; i++)
+                    text += TAB;
+                text += line + '\n';
+            } else if (line.endsWith("{")) {
+                for (int i = 0; i < tabIndex; i++)
+                    text += TAB;
+                tabIndex++;
+                text += line + '\n';
+            } else {
+                for (int i = 0; i < tabIndex; i++)
+                    text += TAB;
+                text += line + '\n';
             }
-            text += line + '\n';
+            if ((line.startsWith("if") || line.startsWith("for")) && !line.endsWith("{")) {
+                text += TAB;
+            }
         }
         return text;
     }
